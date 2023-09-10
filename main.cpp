8cedb94598cb774335f75c5f0602c2d16eabeb12
2354970 <143269060+2354970@users.noreply.github.com>
2023-09-10 17:13:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    int dim_array=0;
+    cin >> dim_array;
+    float Array[dim_array];
+    for (int i = 0; i < dim_array; ++i) {
+        cin >> Array[i];
+    }
+    cout << FindMax(Array,dim_array) << endl;
+    return 0;
 }
