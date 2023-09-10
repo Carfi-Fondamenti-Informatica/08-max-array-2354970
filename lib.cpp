cef14b308bfcb32fbe58f202a7ad9cb9e59c021d
2354970 <143269060+2354970@users.noreply.github.com>
2023-09-10 17:14:31 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+//
+// Created by G.Careddu on 10/09/2023.
+//
 
+#include "lib.h"
+
+float FindMax(float Array[], int dim_array) {
+    float max_value = Array[0];
+    for (int i = 0; i < dim_array; ++i) {
+        if (Array[i]>max_value) {
+            max_value=Array[i];
+        }
+    }
+    return max_value;
+}
